a82662cbdfff3c0483355020b4b43a5c32e0b2bf
Osman Tunahan İNCİRKUŞ <osmantunahanincirkus@gmail.com>
2023-02-24 08:11:47 +0300

    config/databaseConnection added
    config/databaseConnection added

---
+++ config/databaseConnection.js
@@ -0,0 +1,14 @@
+const mongoose= require("mongoose")
+
+
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+    'useNewUrlParser': true,
+    'useUnifiedTopology': true
+})
+    .then(() => {
+        // console.log("Successfully Connected to Database !");
+    })
+    .catch((err) => {
+        // console.log("Failed to Connect to Database ! : " + err);
+    })
