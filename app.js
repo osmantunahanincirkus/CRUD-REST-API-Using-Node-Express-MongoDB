f6021f9d393eb0b23082c849688c9056dad15869
Osman Tunahan İNCİRKUŞ <osmantunahanincirkus@gmail.com>
2023-02-24 09:05:34 +0300

    app.js was organized by adding the necessary requirements and uses
    app.js was organized by adding the necessary requirements and uses

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+require("dotenv").config()
+require("./src/config/databaseConnection")
+const {errorConverter, errorHandler} = require("./src/middlewares/error");
+const {ApiRouter} = require("./src/routers/api");
+const {ApiError} = require("./src/utils/api.error");
+
+app.use(express.json());
+app.use("/api", ApiRouter);
+app.get("/", (req,res) => {
+    res.send("Welcome...");
+});
+app.use((req,res,next)=>{
+    next(new ApiError(404, 'Page not found !'))
+})
+app.use(errorConverter);
+app.use(errorHandler);
+
+module.exports = app;
